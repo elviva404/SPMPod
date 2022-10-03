6fb113bd1471b0912cf8ed9f7c20e11d4e8317d7
Elikem Savie <elikem@menaget.com>
2022-10-03 21:19:16 +0000

    Initial Commit

---
+++ SPMPod/SPMPod.h
@@ -0,0 +1,18 @@
+//
+//  SPMPod.h
+//  SPMPod
+//
+//  Created by Elikem Savie on 03/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMPod.
+FOUNDATION_EXPORT double SPMPodVersionNumber;
+
+//! Project version string for SPMPod.
+FOUNDATION_EXPORT const unsigned char SPMPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMPod/PublicHeader.h>
+
+
